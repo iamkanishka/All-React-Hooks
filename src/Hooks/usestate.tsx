bec52fed008b626660727174e62816c09ed04a72
iamkanishka <kanishkabc123@gmail.com>
2023-05-01 11:01:11 +0530

    useStateExample with counter example

---
+++ src/Hooks/usestate.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function UseState() {
+    const [counter, setCounter] = useState(0);
+     const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    return (
+      <div>
+            <p>Hello world</p>
+              {counter}
+
+            <button onClick={increment}>click counter</button>
+     </div>
+
+
+    );
+}
+
+export default UseState;
